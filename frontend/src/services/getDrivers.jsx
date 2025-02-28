bd2bd92cb0cd20e8074c8dc92a9820517cd257bf
it-smt <smt_razrab@mail.ru>
2025-02-28 23:34:42 +0500

    update

--- frontend/src/services/getDrivers.jsx
+++ frontend/src/services/getDrivers.jsx
@@ -1,6 +1,6 @@
 export async function getDrivers(setDrivers, rankTitle) {
 	await fetch(
-		`/api/v1/main/drivers?rank_name=${rankTitle}`
+		`http://212.67.13.120:8000/api/v1/main/drivers?rank_name=${rankTitle}`
 	)
 		.then(res => res.json())
 		.then(data => {
--- frontend/src/services/getRanks.jsx
+++ frontend/src/services/getRanks.jsx
@@ -1,5 +1,5 @@
 export async function getRanks(setRanks) {
-	await fetch("/api/v1/main/ranks")
+	await fetch("http://212.67.13.120:8000/api/v1/main/ranks")
 		.then(res => res.json())
 		.then(data => {
 			setRanks(data);
